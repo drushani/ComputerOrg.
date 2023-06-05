23a485774133b760cc24418bcd2086963c116e19
drushani <102195368+drushani@users.noreply.github.com>
2023-06-05 19:40:15 +0800

    update tb

--- final/tb_SingleCycle.v
+++ final/tb_SingleCycle.v
@@ -42,8 +42,8 @@ module tb_Pipelined();
 			else if ( CPU.funct == 6'd42 ) $display( "%d, SLT\n", $time );
 			else if ( CPU.funct == 6'd25 ) $display( "%d, MULTU\n", $time );
 			else if ( CPU.funct == 6'd1  ) $display( "%d, MADDU\n", $time );
-			else if ( CPU.funct == 6'd10 ) $display( "%d, MFHI\n", $time );
-			else if ( CPU.funct == 6'd12 ) $display( "%d, MFLO\n", $time );
+			else if ( CPU.funct == 6'd16 ) $display( "%d, MFHI\n", $time );
+			else if ( CPU.funct == 6'd18 ) $display( "%d, MFLO\n", $time );
 		end
 		else if ( CPU.opcode == 6'd9 ) $display( "%d, ADDIU\n", $time );
 		else if ( CPU.opcode == 6'd35 ) $display( "%d, LW\n", $time );
